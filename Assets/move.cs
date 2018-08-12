e66d98b3ac865fa9c662d9553dd4a1e48c219721
EricYC <aerchit@gmail.com>
2018-08-13 06:45:06 +0800

    first commit

---
+++ Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class move : MonoBehaviour {
+
+    public Camera myCam;
+    //public float dist = 0;
+    public FirstPersonController fps;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var dist = Vector3.Distance(Camera.main.transform.position, this.transform.position);
+        transform.position = new Vector3(0,dist,0);
+
+    }
+}
